dbe94b97ead1c239cfc672eaf326323b3244a52b
KiV <ivk15052007@gmail.com>
2022-02-19 20:26:43 +0900

    beta 1.2 particles

--- main.py
+++ main.py
@@ -2,17 +2,70 @@ import pygame
 import os
 import sys
 import json
+import random
 
 file = open('data/maps/progress.json')
 try:
-    level = json.load(file)['level']
+    level = int(json.load(file)['level'])
 except Exception:
     file.close()
     with open('data/maps/progress.json', 'w') as file:
         json.dump({'level': 1}, file)
         level = 1
 
+pygame.init()
+screen_size = (1050, 800)
+screen = pygame.display.set_mode(screen_size)
+FPS = 50
+
+
+class SpriteGroup(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+
+    def get_event(self, event):
+        for sprite in self:
+            sprite.get_event(event)
+
+
 clock_counter = 0
+
+sprite_groups = [
+        tile_group := SpriteGroup(),
+        end_portal := SpriteGroup(),
+        portal := SpriteGroup(),
+        active_walls := SpriteGroup(),
+        buttons := SpriteGroup(),
+        hero_group := SpriteGroup()
+    ]
+particles_groups = [
+        portal_particles := SpriteGroup(),
+        player_particles := SpriteGroup()
+    ]
+portal_list = []
+buttons_list = []
+active_walls_list = []
+
+
+def groups_load():
+    global sprite_groups, tile_group, hero_group, end_portal, portal, active_walls, buttons
+    global portal_list, buttons_list, active_walls_list
+    global particles_groups, portal_particles, player_particles
+    sprite_groups = [
+        tile_group := SpriteGroup(),
+        end_portal := SpriteGroup(),
+        portal := SpriteGroup(),
+        active_walls := SpriteGroup(),
+        buttons := SpriteGroup(),
+        hero_group := SpriteGroup()
+    ]
+    particles_groups = [
+        portal_particles := SpriteGroup(),
+        player_particles := SpriteGroup()
+    ]
+    portal_list = []
+    buttons_list = []
+    active_walls_list = []
 
 
 def load_image(name, color_key=None):
@@ -31,30 +84,12 @@ def load_image(name, color_key=None):
 
 
 def next_level():
-    global level
-    global level_map
-    global hero
-    global max_x, max_y
-    global tile_images
-    global sprite_groups, sprite_group, hero_group, end_portal, portal, active_walls, buttons
-    global portal_list, buttons_list, active_walls_list
+    global hero, level, max_x, max_y
+    global level_map, level
     with open('data/maps/progress.json', 'w') as file:
         level += 1
-        if level > 7:
-            level = 'end screen'
-        else:
-            json.dump({"level": level}, file)
-    sprite_groups = [
-        sprite_group := SpriteGroup(),
-        hero_group := SpriteGroup(),
-        end_portal := SpriteGroup(),
-        portal := SpriteGroup(),
-        active_walls := SpriteGroup(),
-        buttons := SpriteGroup()
-    ]
-    portal_list = []
-    buttons_list = []
-    active_walls_list = []
+        json.dump({'level': level}, file)
+    groups_load()
     try:
         tile_images['empty'] = load_image(f'sprites/free{level}.png')
         level_map = load_level(f"maps/map{level}.map")
@@ -66,11 +101,6 @@ def next_level():
         print('not found')
         sys.exit()
 
-
-pygame.init()
-screen_size = (1050, 800)
-screen = pygame.display.set_mode(screen_size)
-FPS = 50
 
 tile_images = {
     'wall': load_image('sprites/wall.png'),
@@ -84,15 +114,6 @@ class ScreenFrame(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.rect = (0, 0, 500, 500)
-
-
-class SpriteGroup(pygame.sprite.Group):
-    def __init__(self):
-        super().__init__()
-
-    def get_event(self, event):
-        for sprite in self:
-            sprite.get_event(event)
 
 
 class Sprite(pygame.sprite.Sprite):
@@ -204,11 +225,9 @@ class Button(Sprite):
         pos_set = hero_poses & buttons_poses
         if pos_set and all(map(lambda x: x[1] == 0, buttons_list)):
             buttons_list[0][1] = 1
-            print(1)
         elif not pos_set and any(map(lambda x: x[1] == 1, buttons_list)):
             for ind, butt in enumerate(buttons_list):
                 buttons_list[ind][1] = 0
-            print(0)
 
 
 class ActiveWall(Sprite):
@@ -218,40 +237,116 @@ class ActiveWall(Sprite):
         self.rect = self.image.get_rect().move(
             tile_width * pos_x, tile_height * pos_y)
         self.active = 0
+        self.pos = (pos_x, pos_y)
 
     def update(self):
         global active_walls
         if any(map(lambda x: x[1] == 1, buttons_list)) and self.active == 0:
             for x, y in active_walls_list:
                 self.image = load_image(f'sprites/free{level}.png')
+                create_player_particles(self.pos, 'wall')
                 level_map[y][x] = "."
                 self.active = 1
         elif all(map(lambda x: x[1] == 0, buttons_list)) and self.active == 1:
             for x, y in active_walls_list:
                 self.image = load_image('sprites/active_wall.png')
+                create_portal_particles(self.pos)
                 level_map[y][x] = "/"
                 self.active = 0
+
+
+class PortalParticle(Sprite):
+    particles_images = []
+    for x in range(1, 8):
+        particles_images.append(load_image(f"sprites/particles/portal particles/particle{x}.png"))
+    particles = []
+    for x in range(7):
+        scale = random.choice(range(3, 7))
+        particles.append(pygame.transform.scale(particles_images[x], (scale, scale)))
+
+    def __init__(self, pos, dx, dy):
+        super().__init__(portal_particles)
+        self.image = random.choice(self.particles)
+        self.rect = self.image.get_rect()
+        self.velocity = [dx, dy]
+        self.rect.x, self.rect.y = pos
+        self.updates = 0
+
+    def update(self):
+        self.updates += 1
+        self.rect.x += self.velocity[0]
+        self.rect.y += self.velocity[1]
+        if self.updates >= 7:
+            self.kill()
+
+
+def create_portal_particles(position):
+    x, y = position
+    pos = [x * tile_width + 24, y * tile_width + 24]
+    particle_count = random.randint(1, 3)
+    numbers = range(-5, 5)
+    for _ in range(particle_count):
+        portal_particles.add(PortalParticle(pos, random.choice(numbers), random.choice(numbers)))
+
+
+class PlayerParticle(Sprite):
+    particles_image = load_image(f"sprites/particles/player particles/player particle.png")
+    particles = []
+    for scale in range(3, 7):
+        particles.append(pygame.transform.scale(particles_image, (scale, scale)))
+
+    def __init__(self, pos, dx, dy):
+        super().__init__(portal_particles)
+        self.image = random.choice(self.particles)
+        self.rect = self.image.get_rect()
+        self.velocity = [dx, dy]
+        self.rect.x, self.rect.y = pos
+        self.updates = 0
+
+    def update(self):
+        self.updates += 1
+        self.rect.x += round(self.velocity[0] * 0.5)
+        self.rect.y += round(self.velocity[1] * 0.5)
+        if self.updates >= 12:
+            self.kill()
+
+
+def create_player_particles(position, movement):
+    x, y = position
+    if movement == 'up':
+        pos = [x * tile_width + 24, y * tile_width]
+        numbers_x = range(-5, 5)
+        numbers_y = range(0, 13)
+
+    elif movement == 'down':
+        pos = [x * tile_width + 24, (y + 1) * tile_width]
+        numbers_x = range(-5, 5)
+        numbers_y = range(-13, 0)
+
+    elif movement == 'left':
+        pos = [x * tile_width, y * tile_width + 24]
+        numbers_x = range(0, 13)
+        numbers_y = range(-5, 5)
+
+    elif movement == 'right':
+        pos = [(x + 1) * tile_width, y * tile_width + 24]
+        numbers_x = range(-13, 0)
+        numbers_y = range(-5, 5)
+
+    else:
+        pos = [x * tile_width + 24, y * tile_width + 24]
+        numbers_x = range(-5, 5)
+        numbers_y = range(-5, 5)
+
+    particle_count = random.randint(3, 7)
+    for _ in range(particle_count):
+        player_particles.add(PlayerParticle(pos, random.choice(numbers_x), random.choice(numbers_y)))
 
 
 player = None
 running = True
 clock = pygame.time.Clock()
-sprite_groups = [
-    tile_group := SpriteGroup(),
-    end_portal := SpriteGroup(),
-    portal := SpriteGroup(),
-    active_walls := SpriteGroup(),
-    buttons := SpriteGroup(),
-    hero_group := SpriteGroup()
-]
-portal_list = []
-buttons_list = []
-active_walls_list = []
-
-
-def terminate():
-    pygame.quit()
-    sys.exit
+groups_load()
 
 
 def start_screen():
@@ -282,7 +377,8 @@ def start_screen():
         screen.blit(screen2, (0, 0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                terminate()
+                pygame.quit()
+                sys.exit()
             elif event.type == pygame.KEYDOWN or \
                     event.type == pygame.MOUSEBUTTONDOWN:
                 return
@@ -328,6 +424,7 @@ def generate_level(level):
             elif level[y][x] == '%':
                 end_portal.add(AnimatedSpriteEndPortal(load_image('sprites/end_portal_animate.png'), 6, 2, x, y))
                 level_map[y][x] = "%"
+                portal_list.append((10, (x, y)))
 
             elif level[y][x] in '0123456789':
                 portal.add(AnimatedSpritePortal(load_image('sprites/portal_animate.png'), 4, 2, x, y, int(level[y][x])))
@@ -361,9 +458,11 @@ def move(hero, movement):
     if movement == "up":
         if y > 0 and level_map[y - 1][x] == ".":
             hero.move(x, y - 1)
+            create_player_particles((x, y), movement)
 
         elif y > 0 - 1 and level_map[y - 1][x] == "%":
             hero.move(x, y - 1)
+            create_player_particles((x, y), movement)
             next_level()
 
         elif y > 0 - 1 and level_map[y - 1][x] in "0123456789":
@@ -372,18 +471,22 @@ def move(hero, movement):
                 x, y = portal_list[portal_index + 1][1]
             else:
                 x, y = portal_list[portal_index - 1][1]
-            if level_map[y - 1][x] == ".":
+            if level_map[y - 1][x] in ".*":
                 hero.move(x, y - 1)
+                create_player_particles((x, y), movement)
 
         elif y > 0 - 1 and level_map[y - 1][x] == "*":
             hero.move(x, y - 1)
+            create_player_particles((x, y), movement)
 
     elif movement == "down":
         if y < max_y - 1 and level_map[y + 1][x] == ".":
             hero.move(x, y + 1)
+            create_player_particles((x, y), movement)
 
         elif y < max_y - 1 and level_map[y + 1][x] == "%":
             hero.move(x, y + 1)
+            create_player_particles((x, y), movement)
             next_level()
 
         elif y < max_y - 1 and level_map[y + 1][x] in "0123456789":
@@ -392,47 +495,61 @@ def move(hero, movement):
                 x, y = portal_list[portal_index + 1][1]
             else:
                 x, y = portal_list[portal_index - 1][1]
-            if level_map[y + 1][x] == ".":
+            if level_map[y + 1][x] in ".*":
                 hero.move(x, y + 1)
+                create_player_particles((x, y), movement)
 
         elif y < max_y - 1 and level_map[y + 1][x] == "*":
             hero.move(x, y + 1)
+            create_player_particles((x, y), movement)
 
     elif movement == "left":
         if x > 0 and level_map[y][x - 1] == ".":
             hero.move(x - 1, y)
+            create_player_particles((x, y), movement)
+
         elif x > 0 - 1 and level_map[y][x - 1] == "%":
             hero.move(x - 1, y)
+            create_player_particles((x, y), movement)
             next_level()
+
         elif x > 0 - 1 and level_map[y][x - 1] in "0123456789":
             portal_index = int(level_map[y][x - 1])
             if portal_index % 2 == 0:
                 x, y = portal_list[portal_index + 1][1]
             else:
                 x, y = portal_list[portal_index - 1][1]
-            if level_map[y][x - 1] == ".":
+            if level_map[y][x - 1] in ".*":
                 hero.move(x - 1, y)
+                create_player_particles((x, y), movement)
 
         elif x > 0 and level_map[y][x - 1] == "*":
             hero.move(x - 1, y)
+            create_player_particles((x, y), movement)
 
     elif movement == "right":
         if x < max_x - 1 and level_map[y][x + 1] == ".":
             hero.move(x + 1, y)
+            create_player_particles((x, y), movement)
+
         elif x < max_x - 1 and level_map[y][x + 1] == "%":
             hero.move(x + 1, y)
+            create_player_particles((x, y), movement)
             next_level()
+
         elif x < max_x - 1 and level_map[y][x + 1] in "0123456789":
             portal_index = int(level_map[y][x + 1])
             if portal_index % 2 == 0:
                 x, y = portal_list[portal_index + 1][1]
             else:
                 x, y = portal_list[portal_index - 1][1]
-            if level_map[y][x + 1] == ".":
+            if level_map[y][x + 1] in ".*":
                 hero.move(x + 1, y)
+                create_player_particles((x, y), movement)
 
         elif x < max_x - 1 and level_map[y][x + 1] == "*":
             hero.move(x + 1, y)
+            create_player_particles((x, y), movement)
 
 
 start_screen()
@@ -455,8 +572,13 @@ while running:
             elif event.key == pygame.K_RIGHT:
                 for hero in hero_group:
                     move(hero, "right")
+            elif event.key == pygame.K_ESCAPE:
+                level -= 1
+                next_level()
     screen.fill(pygame.Color("black"))
     for group in sprite_groups:
+        group.draw(screen)
+    for group in particles_groups:
         group.draw(screen)
     if clock_counter % 5 == 0:
         active_walls.update()
@@ -465,6 +587,11 @@ while running:
         buttons.update()
         end_portal.update()
         portal.update()
+    if clock_counter % 2 == 0:
+        for portal1 in portal_list:
+            create_portal_particles(portal1[1])
+        portal_particles.update()
+    player_particles.update()
     clock.tick(FPS)
     pygame.display.flip()
     clock_counter += 1
